bc65cdf87075b197beb53ab3a4cdb2410ffa4528
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-10-15 15:32:44 +0100

    Added to Passenger Controller

--- src/main/java/ie/atu/cicd_week5/controller/PassengerController.java
+++ src/main/java/ie/atu/cicd_week5/controller/PassengerController.java
@@ -1,4 +1,45 @@
 package ie.atu.cicd_week5.controller;
 
+import ie.atu.cicd_week5.model.Passenger;
+import ie.atu.cicd_week5.service.PassengerService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/passengers")
 public class PassengerController {
+
+    private final PassengerService service;
+
+    public PassengerController(PassengerService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Passenger>> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Passenger> getOne(@PathVariable String id) {
+        Optional<Passenger> maybe = service.findById(id);
+        if (maybe.isPresent()) {
+            return ResponseEntity.ok(maybe.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Passenger> create( @Valid @RequestBody Passenger p) {
+        Passenger created = service.create(p);
+        return ResponseEntity
+                .created(URI.create("/api/passengers/" + created.getPassengerId()))
+                .body(created);
+    }
 }
