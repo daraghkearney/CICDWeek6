d0b720e57662cce1db0f1f7260b364fdd209af82
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-10-15 14:50:53 +0100

    Adjustments to pom

--- src/main/java/ie/atu/cicd_week5/model/Passenger.java
+++ src/main/java/ie/atu/cicd_week5/model/Passenger.java
@@ -1,4 +1,14 @@
 package ie.atu.cicd_week5.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email;
+
+@Data @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Passenger {
+
 }
