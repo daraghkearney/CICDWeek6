7b272d626b9989e59f9faa6e9f12b293c9684b90
daraghkearney <g00438215@atu.ie>
2025-10-17 09:07:52 +0100

    Fixed Class Structure

--- src/main/java/ie/atu/cicd_week5/CicdWeek5Application.java
+++ src/main/java/ie/atu/cicd_week5/CicdWeek5Application.java
@@ -1,4 +1,4 @@
-package ie.atu.cicd_week5.service;
+package ie.atu.cicd_week5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
